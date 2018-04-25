14c5d81aa3b5acb94eeb67eadd18935ce26a8fc1
johnruben <jruben@roostify.com>
2018-04-25 13:21:42 -0700

    service update script

--- awsTaskDefinition.sh
+++ awsTaskDefinition.sh
@@ -4,3 +4,5 @@ TASK_REVISION=`aws ecs describe-task-definition --task-definition $1 --region $3
 #DESIRED_COUNT=`aws ecs describe-services --services $2 --region $3 | egrep "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
 echo $TASK_REVISION
 #echo $DESIRED_COUNT
+
+aws ecs update-service --cluster default --service $2 --task-definition $2:${TASK_REVISION} --desired-count 1
